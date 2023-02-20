9bfb807aaa4accf8b86843f8b7ddf1b2575f34f6
kelydev <kely.piscis.97@gmail.com>
2023-02-20 17:53:57 -0500

    Refactoring Code

--- Semana_01/ProductManager.js
+++ Semana_01/ProductManager.js
@@ -18,41 +18,42 @@ class ProductManager
 
         if (!product.title || !product.description || !product.thumbnail || !product.code || !product.stock) 
         {
-            console.log('Todos los campos son obligatorios');
+            return console.log('Todos los campos son obligatorios');
         }
-        else if(existingProductCode)
-        {
-            console.log(`El producto con código ${product.code} ya existe`);
-        }
-        else if(existingProduct)
-        {
-            console.log(`El producto con id ${product.id} ya existe`);
-        }
-        else
-        {
-            const newProduct = { 
-                ...product,
-                id: ++this.Id 
-            };
-            this.products.push(newProduct);
-            return newProduct;
-        }
-    }
 
+        if(existingProductCode)
+        {
+            return console.log(`El producto con código ${product.code} ya existe`);
+        }
+
+        if(existingProduct)
+        {
+            return console.log(`El producto con id ${product.id} ya existe`);
+        }
+    
+        const newProduct = 
+        { 
+            ...product,
+            id: ++this.Id 
+        };
+
+        this.products.push(newProduct);
+        return newProduct;
+    }
 
     getProducts()
     {
         return this.products;
     }
 
-    getProductById(id) 
+    getProductById(id)
     {
         const product = this.products.find(product => product.id === id);
         if(!product)
         {
             return 'Producto no encontrado';
         }
-        return product
+        return product;
     }
 }
 
@@ -83,7 +84,7 @@ console.log(producto.getProducts());
 
 
 // --------------------- 04. Id generado automaticamente para cada producto ------------------*/
-
+/*
 const product1 = { title: "producto prueba", description: "Este es un producto prueba1", thumbnail:"Sin imagen", code:"abc123", stock:25};
 const product2 = { title: "producto prueba", description: "Este es un producto prueba2", thumbnail:"Sin imagen", code:"abc124", stock:25};
 const product3 = { title: "producto prueba", description: "Este es un producto prueba3", thumbnail:"Sin imagen", code:"abc125", stock:25};
@@ -93,16 +94,16 @@ producto.addProduct(product2);
 producto.addProduct(product3);
 
 console.log(producto.getProducts());
-
+*/
 
 
 // --------------------- 05. Buscar un producto por el ID ------------------
-/*
+
 const product1 = { title: "producto prueba", description: "Este es un producto prueba1", thumbnail:"Sin imagen", code:"abc123", stock:25};
 const product2 = { title: "producto prueba", description: "Este es un producto prueba2", thumbnail:"Sin imagen", code:"abc124", stock:25};
 
 producto.addProduct(product1);
 producto.addProduct(product2);
  
-console.log(producto.getProductById(1));
-*/
+console.log(producto.getProductById(3));
+
